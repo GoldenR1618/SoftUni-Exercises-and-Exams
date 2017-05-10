d07bd219a8579715497aa5c39803e0101b14dd0d
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-10 23:39:46 +0300

    Minor changes.

---
+++ 01.Learning_C_Sharp/Матрици/Матрици.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Матрици
+{
+    class Матрици
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
