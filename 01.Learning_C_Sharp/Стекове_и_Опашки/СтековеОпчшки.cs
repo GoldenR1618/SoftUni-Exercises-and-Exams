5e2a3367d1ce7de63d3202a6b3ad26ecfb8aa9a0
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-10 23:31:52 +0300

    Add some files.

---
+++ 01.Learning_C_Sharp/Стекове_и_Опашки/СтековеОпчшки.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Стекове_и_Опашки
+{
+    class СтековеОпчшки
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
