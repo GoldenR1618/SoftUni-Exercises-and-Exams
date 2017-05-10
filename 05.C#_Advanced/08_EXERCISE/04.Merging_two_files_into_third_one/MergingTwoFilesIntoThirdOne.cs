21d6d2c4d6815077216949f04c95f39286ac213a
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-10 22:34:38 +0300

    Add FILES_AND_DIRECTORIES_Exercise.

---
+++ 05.C#_Advanced/08_EXERCISE/04.Merging_two_files_into_third_one/MergingTwoFilesIntoThirdOne.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.Merging_two_files_into_third_one
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MergingTwoFilesIntoThirdOne
+    {
+        public static void Main(string[] args)
+        {
+            List<string> fileOne = File.ReadAllText("FileOne.txt").Split(new char[] { '\n', ',', ';', ':', '.', '!',
+                    '(', ')', '"', '-', '\\', '/', '[', ']', ' ',
+                    '?', '\r', '`', '_', '{', '}', '<', '>', '\'' },
+                    StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> fileTwo = File.ReadAllText("FileTwo.txt").Split(new char[] { '\n', ',', ';', ':', '.', '!',
+                    '(', ')', '"', '-', '\\', '/', '[', ']', ' ',
+                    '?', '\r', '`', '_', '{', '}', '<', '>', '\'' },
+                    StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            int count = Math.Min(fileOne.Count, fileTwo.Count);
+
+            for (int i = 0; i < count; i++)
+            {
+                File.AppendAllText("Output.txt", fileOne[i] + Environment.NewLine);
+                File.AppendAllText("Output.txt", fileTwo[i] + Environment.NewLine);
+            }
+        }
+    }
+}
