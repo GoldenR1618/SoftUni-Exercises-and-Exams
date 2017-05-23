92d4fdbbf8a08fef1fff793fb2a092ad0c2775e8
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-23 16:15:14 +0300

    Edit file structore.

---
+++ 01.Learning_C_Sharp/Matrices/Matrices.cs
@@ -0,0 +1,16 @@
+﻿namespace Matrices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Matrices
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
