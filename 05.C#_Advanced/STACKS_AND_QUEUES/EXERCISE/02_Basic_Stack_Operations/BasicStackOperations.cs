2e37e0c196cd7b954adc2350a7e1743a05895208
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-23 15:31:58 +0300

    Edit file structore.

---
+++ 05.C#_Advanced/STACKS_AND_QUEUES/EXERCISE/02_Basic_Stack_Operations/BasicStackOperations.cs
@@ -0,0 +1,51 @@
+﻿namespace _02_Basic_Stack_Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BasicStackOperations
+    {
+        public static void Main(string[] args)
+        {
+            List<int> nsx = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            int n = nsx[0];
+            int s = nsx[1];
+            int x = nsx[2];
+
+            int[] nums = new int[n];
+            nums = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> numbers = new Stack<int>();
+
+            foreach (var item in nums)
+            {
+                numbers.Push(item);
+            }
+
+            for (int i = 1; i <= s; i++)
+            {
+                numbers.Pop();
+            }
+
+            bool isThere = numbers.Contains(x);
+
+            if (isThere)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (numbers.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+        }
+    }
+}
---
+++ 05.C#_Advanced/STACKS_AND_QUEUES/EXERCISE/08_Recursive_Fibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,34 @@
+﻿namespace _08_Recursive_Fibonacci
+{
+    using System;
+
+    public class RecursiveFibonacci
+    {
+        public static long[] fibonacci;
+
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            fibonacci = new long[n];
+            fibonacci[0] = 1;
+
+            if (n > 1)
+            {
+                fibonacci[1] = 1;
+            }
+
+            Console.WriteLine(GetFibonacci(n - 1));
+        }
+
+        public static long GetFibonacci(int n)
+        {
+            if (fibonacci[n] == 0)
+            {
+                fibonacci[n] = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            }
+
+            return fibonacci[n];
+        }
+    }
+}
