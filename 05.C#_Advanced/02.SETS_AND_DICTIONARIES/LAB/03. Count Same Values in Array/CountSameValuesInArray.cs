778826e15d4300b0e2391ff38df9b977babfe55e
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-25 16:50:32 +0300

    Add LAB in SETS_AND_DICTIONARIES.

---
+++ 05.C#_Advanced/02.SETS_AND_DICTIONARIES/LAB/03. Count Same Values in Array/CountSameValuesInArray.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.Count_Same_Values_in_Array
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class CountSameValuesInArray
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double[] numbers = input.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(n => double.Parse(n, CultureInfo.InvariantCulture)).ToArray();
+
+            var dictionary = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!dictionary.ContainsKey(number))
+                {
+                    dictionary.Add(number, 1);
+                }
+                else
+                {
+                    dictionary[number]++;
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
