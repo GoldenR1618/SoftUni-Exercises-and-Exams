582324de7d0b7111da7b981c6ccf8006836969ad
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-29 12:42:58 +0300

    Add BashSoft-FirstWeek

---
+++ 05.C#_Advanced/BashSoft/BashSoft/BashSoftProgram.cs
@@ -0,0 +1,31 @@
+﻿namespace BashSoft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BashSoftProgram
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine;
+            int initialIdentation = path.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count != 0)
+            {
+                subFolders.Dequeue();
+                OutputWriter.WriteMessageOnNewLine(path);
+                subFolders.Enqueue(path);
+            }
+        }
+    }
+}
---
+++ 05.C#_Advanced/BashSoft/BashSoft/OutputWriter.cs
@@ -0,0 +1,34 @@
+﻿namespace BashSoft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class OutputWriter
+    {
+        public static void WriteMessage(string message)
+        {
+            Console.Write(message);
+        }
+
+        public static void WriteMessageOnNewLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void WriteEmptyLine()
+        {
+            Console.WriteLine();
+        }
+
+        public static void DisplayException(string message)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = currentColor;
+        }
+    }
+}
