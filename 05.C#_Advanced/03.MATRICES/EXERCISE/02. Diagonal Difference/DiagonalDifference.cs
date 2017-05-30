fee61afb8559311c8fb93be6119892117d10fd80
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-31 02:06:19 +0300

    Add MatrixOfPalindromes.cs

--- 05.C#_Advanced/03.MATRICES/EXERCISE/02. Diagonal Difference/DiagonalDifference.cs
+++ 05.C#_Advanced/03.MATRICES/EXERCISE/02. Diagonal Difference/DiagonalDifference.cs
@@ -1,16 +1,38 @@
 ﻿namespace _02.Diagonal_Difference
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class DiagonalDifference
     {
         public static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            long[][] matrix = new long[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            }
+
+            //Primary diagonal:
+            long primarySum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                primarySum += matrix[row][row];
+            }
+
+            //Secondary diagonal:
+            long secondarySum = 0;
+
+            for (int row = 0, col = n - 1; row < n; row++, col--)
+            {
+                secondarySum += matrix[row][col];
+            }
+
+            Console.WriteLine(Math.Abs(primarySum - secondarySum));
         }
     }
 }
