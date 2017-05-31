cb6384b18173e7da3454e35dbcf6e9fffdbe17e8
Ilian Damianov <ilian.damianov@gmail.com>
2017-05-31 23:15:37 +0300

    Add SquaresInMatrix, MaximalSum, StringMatrixRotation.

--- 05.C#_Advanced/03.MATRICES/EXERCISE/04. Maximal Sum/MaximalSum.cs
+++ 05.C#_Advanced/03.MATRICES/EXERCISE/04. Maximal Sum/MaximalSum.cs
@@ -1,16 +1,69 @@
 ﻿namespace _04.Maximal_Sum
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class MaximalSum
     {
         public static void Main(string[] args)
         {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int r = input[0];
+            int c = input[1];
 
+            int[,] matrix = new int[r, c];
+
+            for (int row = 0; row < r; row++)
+            {
+                int[] cells = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < c; col++)
+                {
+                    matrix[row, col] = cells[col];
+                }
+            }
+
+            long maxSum = 0;
+            long currentSum = 0;
+            int[,] resultMatrix = new int[3, 3];
+
+            for (int row = 0; row < r - 2; row++)
+            {
+                for (int col = 0; col < c - 2; col++)
+                {
+                    currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                        matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                        matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+
+                        resultMatrix[0, 0] = matrix[row, col];
+                        resultMatrix[0, 1] = matrix[row, col + 1];
+                        resultMatrix[0, 2] = matrix[row, col + 2];
+                        resultMatrix[1, 0] = matrix[row + 1, col];
+                        resultMatrix[1, 1] = matrix[row + 1, col + 1];
+                        resultMatrix[1, 2] = matrix[row + 1, col + 2];
+                        resultMatrix[2, 0] = matrix[row + 2, col];
+                        resultMatrix[2, 1] = matrix[row + 2, col + 1];
+                        resultMatrix[2, 2] = matrix[row + 2, col + 2];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write(resultMatrix[row, col] + " ");
+                }
+
+                Console.WriteLine(resultMatrix[row, 2]);
+            }
         }
     }
 }
+
--- 05.C#_Advanced/03.MATRICES/EXERCISE/12. String Matrix Rotation/StringMatrixRotation.cs
+++ 05.C#_Advanced/03.MATRICES/EXERCISE/12. String Matrix Rotation/StringMatrixRotation.cs
@@ -2,15 +2,134 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class StringMatrixRotation
     {
         public static void Main(string[] args)
         {
+            string[] comm = Console.ReadLine().Split(new char[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            string line = Console.ReadLine();
 
+            List<string> lines = new List<string>();
+
+            int command = int.Parse(comm[1]);
+
+            while (command >= 360)
+            {
+                command -= 360;
+            }
+
+            int longestLine = 0;
+
+            while (line != "END")
+            {
+                lines.Add(line);
+
+                if (line.Length > longestLine)
+                {
+                    longestLine = line.Length;
+                }
+
+                line = Console.ReadLine();
+            }
+
+            List<string> modLines = new List<string>();
+
+            foreach (var item in lines)
+            {
+                modLines.Add(item.PadRight(longestLine));
+            }
+
+            char[,] matrix = new char[modLines.Count, longestLine];
+
+            for (int row = 0; row < modLines.Count; row++)
+            {
+                for (int col = 0; col < longestLine; col++)
+                {
+                    matrix[row, col] = modLines[row][col];
+                }
+            }
+
+            switch (command)
+            {
+                case 0:
+                    RotateMatrix0(matrix);
+                    break;
+                case 90:
+                    RotateMatrix90(matrix);
+                    break;
+                case 180:
+                    RotateMatrix180(matrix);
+                    break;
+                case 270:
+                    RotateMatrix270(matrix);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void RotateMatrix90(char[,] matrix)
+        {
+            char[,] matrix90 = new char[matrix.GetLength(1), matrix.GetLength(0)];
+
+            for (int row = 0, row2 = matrix.GetLength(0) - 1; row < matrix.GetLength(0); row++, row2--)
+            {
+                for (int col = 0, col2 = matrix.GetLength(1) - 1; col < matrix.GetLength(1); col++, col2--)
+                {
+                    matrix90[col, row2] = matrix[row, col];
+                }
+            }
+
+            PrintMatrix(matrix90);
+        }
+
+        public static void RotateMatrix180(char[,] matrix)
+        {
+            char[,] matrix180 = new char[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int row = 0, row2 = matrix.GetLength(0) - 1; row < matrix.GetLength(0); row++, row2--)
+            {
+                for (int col = 0, col2 = matrix.GetLength(1) - 1; col < matrix.GetLength(1); col++, col2--)
+                {
+                    matrix180[row, col] = matrix[row2, col2];
+                }
+            }
+
+            PrintMatrix(matrix180);
+        }
+
+        public static void RotateMatrix270(char[,] matrix)
+        {
+            char[,] matrix270 = new char[matrix.GetLength(1), matrix.GetLength(0)];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0, col2 = matrix.GetLength(1) - 1; col < matrix.GetLength(1); col++, col2--)
+                {
+                    matrix270[col2, row] = matrix[row, col];
+                }
+            }
+
+            PrintMatrix(matrix270);
+        }
+
+        private static void RotateMatrix0(char[,] matrix)
+        {
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine(matrix[row, matrix.GetLength(1) - 1]);
+            }
         }
     }
 }
