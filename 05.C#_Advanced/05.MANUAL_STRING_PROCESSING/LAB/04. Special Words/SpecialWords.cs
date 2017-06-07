af51526c7e94eface53a6fae121b3d8d636a722e
Ilian Damianov <ilian.damianov@gmail.com>
2017-06-07 15:05:35 +0300

    Add MANUAL_STRING_PROCESSING - LAB from 4 and 5.

--- 05.C#_Advanced/05.MANUAL_STRING_PROCESSING/LAB/04. Special Words/SpecialWords.cs
+++ 05.C#_Advanced/05.MANUAL_STRING_PROCESSING/LAB/04. Special Words/SpecialWords.cs
@@ -3,14 +3,33 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class SpecialWords
     {
         public static void Main(string[] args)
         {
+            List<string> specialWords = Console.ReadLine().Split(' ').ToList();
+            List<string> text = Console.ReadLine().Split(new char[] { '\n', ',', ';', ':', '!', '(', ')', '"', '-', '\\', '/', '[', ']', ' ', '?', '\r', '`', '_', '{', '}', '<', '>', '\'' }, StringSplitOptions.RemoveEmptyEntries).ToList();
 
+            Dictionary<string, int> specWordsDict = new Dictionary<string, int>();
+
+            foreach (var item in specialWords)
+            {
+                specWordsDict[item] = 0;
+            }
+
+            foreach (var word in text)
+            {
+                if (specWordsDict.ContainsKey(word))
+                {
+                    specWordsDict[word]++;
+                }
+            }
+
+            foreach (var item in specWordsDict)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
         }
     }
 }
