9413195ffbe461c4b51d6b812a6f6f4aa25d3fbd
Ilian Damianov <ilian.damianov@gmail.com>
2017-06-21 18:03:01 +0300

    Add EXAMS/19.06.2016 - 02, 03, 04 (30/100).

---
+++ 05.C#_Advanced/EXAMS/19.06.2016/01. Cubic Artillery/CubicArtillery.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Cubic_Artillery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CubicArtillery
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 05.C#_Advanced/EXAMS/19.06.2016/04. Cubic Assault/CubicAssault.cs
@@ -0,0 +1,87 @@
+﻿namespace _04.Cubic_Assault
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CubicAssault
+    {
+        public static void Main(string[] args)
+        {
+            //  30/100
+
+            Dictionary<string, Dictionary<string, int>> dictionary = new Dictionary<string, Dictionary<string, int>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Count em all")
+            {
+                string[] inputArr = input.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                string regionName = inputArr[0];
+                string type = inputArr[1];
+                int count = int.Parse(inputArr[2]);
+                
+                if (!dictionary.ContainsKey(regionName))
+                {
+                    dictionary[regionName] = new Dictionary<string, int>
+                    {
+                        ["Black"] = 0,
+                        ["Green"] = 0,
+                        ["Red"] = 0
+                    };
+                }
+
+                if (type == "Green")
+                {
+                    while (count >= 1000000)
+                    {
+                        dictionary[regionName]["Red"]++;
+
+                        count -=1000000;
+                    }
+
+                    if (dictionary[regionName]["Red"] >= 1000000)
+                    {
+                        while (dictionary[regionName]["Red"] >= 1000000)
+                        {
+                            dictionary[regionName]["Black"]++;
+
+                            dictionary[regionName]["Red"] -=1000000;
+                        }
+                    }
+
+                    dictionary[regionName]["Green"] += count;
+                }
+                else if (type == "Red")
+                {
+                    while (count >= 1000000)
+                    {
+                        dictionary[regionName]["Black"]++;
+
+                        count -= 1000000;
+                    }
+
+                    dictionary[regionName]["Red"] += count;
+                }
+                else
+                {
+                    dictionary[regionName]["Black"] += count;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in dictionary.OrderByDescending(x => x.Value["Black"]).ThenBy(x => x.Key.Length).ThenBy(x => x.Key))
+            {
+                Console.WriteLine(item.Key);
+
+                foreach (var i in item.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"-> {i.Key} : {i.Value}");
+                }
+            }
+        }
+    }
+}
