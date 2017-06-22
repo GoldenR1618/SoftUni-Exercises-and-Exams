8495a2a04f33468ff10f0d35e0be7f6b6413e5a2
Ilian Damianov <ilian.damianov@gmail.com>
2017-06-22 22:08:42 +0300

    Add EXAMS/22.08.2016/01. Second Nature/SecondNature.cs

--- 05.C#_Advanced/EXAMS/22.08.2016/01. Second Nature/SecondNature.cs
+++ 05.C#_Advanced/EXAMS/22.08.2016/01. Second Nature/SecondNature.cs
@@ -3,18 +3,78 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class SecondNature
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            List<int> flowers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-            List<int> buckets = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            Stack<int> dust = new Stack<int>
+                (
+                Console.ReadLine()
+                .Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                );
 
+            Stack<int> water = new Stack<int>
+                (
+                Console.ReadLine()
+                .Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                );
 
-            
+            Queue<int> secondNatureFlower = WaterFlowers(dust, water);
+            PrintResult(water, secondNatureFlower, dust);
+        }
+
+        private static Queue<int> WaterFlowers(Stack<int> dust, Stack<int> water)
+        {
+            var secondNatureFlower = new Queue<int>();
+
+            while (dust.Count > 0 && water.Count > 0)
+            {
+                if (water.Peek() < dust.Peek()) // Water is not enough)
+                {
+                    dust.Push(dust.Pop() - water.Pop()); // Update Dust value
+                }
+                else if (water.Peek() == dust.Peek()) // add Second Nature Flower
+                {
+                    secondNatureFlower.Enqueue(dust.Pop());
+                    water.Pop();
+                }
+                else // water is more than needed
+                {
+                    GetBackRemainingWater(water.Pop() - dust.Pop(), water);
+                }
+            }
+
+            return secondNatureFlower;
+        }
+
+        private static void GetBackRemainingWater(int remainintWater, Stack<int> water)
+        {
+            if (remainintWater > 0)
+            {
+                if (water.Any())
+                {
+                    water.Push(water.Pop() + remainintWater);
+                }
+                else
+                {
+                    water.Push(remainintWater);
+                }
+            }
+        }
+
+        private static void PrintResult(Stack<int> water, Queue<int> secondNatureFlower, Stack<int> dust)
+        {
+            Console.WriteLine((water.Any())
+                ? string.Join(" ", water)
+                : string.Join(" ", dust));
+
+            Console.WriteLine((secondNatureFlower.Any())
+                ? string.Join(" ", secondNatureFlower)
+                : "None");
         }
     }
 }
