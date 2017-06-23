b6953d108b7e5ee635ab3f9d0b008d96ea02c0cf
Ilian Damianov <ilian.damianov@gmail.com>
2017-06-24 00:07:53 +0300

    Add: EXAMS/13.03.2016/01. Arrange Numbers/ArrangeNumbers.cs EXAMS/13.03.2016/02. Monopoly/Monopoly.cs

--- 05.C#_Advanced/EXAMS/13.03.2016/01. Arrange Numbers/ArrangeNumbers.cs
+++ 05.C#_Advanced/EXAMS/13.03.2016/01. Arrange Numbers/ArrangeNumbers.cs
@@ -2,15 +2,50 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class ArrangeNumbers
     {
         public static void Main(string[] args)
         {
+            string[] nums = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
 
+            List<string> numNames = new List<string>
+            {
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+            };
+
+            SortedDictionary<string, string> result = new SortedDictionary<string, string>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                string currNum = nums[i];
+                string resNum = "";
+
+                for (int j = 0; j < currNum.Length; j++)
+                {
+                    string currDiggit = currNum[j].ToString();
+
+                    if (j == 0)
+                    {
+                        resNum = resNum + numNames[int.Parse(currDiggit)];
+                    }
+                    else
+                    {
+                        resNum = resNum + "-" + numNames[int.Parse(currDiggit)];
+                    }
+                }
+
+                if (result.ContainsKey(resNum))
+                {
+                    result[resNum] += ", " + currNum;
+                }
+                else
+                {
+                    result.Add(resNum, currNum);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result.Values));
         }
     }
 }
