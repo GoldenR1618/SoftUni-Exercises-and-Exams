bd6a546a98e5637684f3b8352e585d93317c0055
Ilian Damianov <ilian.damianov@gmail.com>
2017-07-02 16:31:43 +0300

    Add ENCAPSULATION\LAB - Problem 3 and 4.

--- 06.C#_OOP_Basics/02.ENCAPSULATION/LAB/04. Team/StartUp.cs
+++ 06.C#_OOP_Basics/02.ENCAPSULATION/LAB/04. Team/StartUp.cs
@@ -8,6 +8,29 @@ public class StartUp
 {
     public static void Main(string[] args)
     {
+        int lines = int.Parse(Console.ReadLine());
+        List<Person> persons = new List<Person>();
 
+        for (int i = 0; i < lines; i++)
+        {
+            try
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        double.Parse(cmdArgs[3]));
+                persons.Add(person);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        Team team = new Team("BestTeam");
+        persons.ForEach(p => team.AddPlayer(p));
+
+        Console.WriteLine(team.ToString());
     }
 }
---
+++ 06.C#_OOP_Basics/02.ENCAPSULATION/LAB/04. Team/Team.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Team
+{
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(string name)
+    {
+        this.Name = name;
+        this.firstTeam = new List<Person>();
+        this.reserveTeam = new List<Person>();
+    }
+
+    public string Name { get { return this.name; } set { this.name = value; } }
+    public IReadOnlyCollection<Person> FirstTeam
+    {
+        get
+        {
+            return this.firstTeam.AsReadOnly();
+        }
+    }
+    public IReadOnlyCollection<Person> ReserveTeam
+    {
+        get
+        {
+            return this.reserveTeam.AsReadOnly();
+        }
+    }
+
+    public void AddPlayer(Person person)
+    {
+        if (person.Age < 40)
+        {
+            firstTeam.Add(person);
+        }
+        else
+        {
+            reserveTeam.Add(person);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"First team have {this.firstTeam.Count} players\nReserve team have {this.reserveTeam.Count} players";
+    }
+}
