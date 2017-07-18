3992d2ef9a74be2d404dddb4b1b78739f649c5b6
Ilian Damianov <ilian.damianov@gmail.com>
2017-07-18 14:23:56 +0300

    Reorganize files.

---
+++ 02.Programming_Basics/EXAM PREPARATION/Exam_ 28.08.2016/01.Profit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
+            double kursDolar = double.Parse(Console.ReadLine());
+            double profitDolara = (((n * m * 12 + n * m * 2.5) * 0.75) / 365);
+
+            Console.WriteLine("{0:f2}", profitDolara * kursDolar);
+        }
+    }
+}
