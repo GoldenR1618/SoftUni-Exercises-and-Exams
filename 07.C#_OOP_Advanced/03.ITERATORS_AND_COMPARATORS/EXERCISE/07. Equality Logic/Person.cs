61db4f3e3393ceb3c436aff02488c6e7ed5668a5
Ilian Damianov <ilian.damianov@gmail.com>
2017-07-28 19:44:06 +0300

    Add ITERATORS_AND_COMPARATORS\EXERCISE - 7.

---
+++ 07.C#_OOP_Advanced/03.ITERATORS_AND_COMPARATORS/EXERCISE/07. Equality Logic/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+internal class Person : IComparable<Person>
+{
+    private string name;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name
+    {
+        get { return this.name; }
+        private set { this.name = value; }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        private set { this.age = value; }
+    }
+
+    public int CompareTo(Person other)
+    {
+        int result = this.Name.CompareTo(other.Name);
+
+        if (result == 0)
+        {
+            result = this.Age.CompareTo(other.Age);
+        }
+
+        return result;
+    }
+
+    public override bool Equals(object obj)
+    {
+        Person otherPerson = (Person)obj;
+    
+        if (this.Name == otherPerson.Name && this.Age == otherPerson.Age)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return (this.Name + this.Age.ToString()).GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Name} {this.Age}";
+    }
+}
--- 07.C#_OOP_Advanced/03.ITERATORS_AND_COMPARATORS/EXERCISE/07. Equality Logic/StartUp.cs
+++ 07.C#_OOP_Advanced/03.ITERATORS_AND_COMPARATORS/EXERCISE/07. Equality Logic/StartUp.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 public class StartUp
 {
     public static void Main()
     {
+        SortedSet<Person> sortedSet = new SortedSet<Person>();
+        HashSet<Person> hashSet = new HashSet<Person>();
 
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] args = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Person currentPerson = new Person(args[0], int.Parse(args[1]));
+
+            sortedSet.Add(currentPerson);
+            hashSet.Add(currentPerson);
+        }
+
+        Console.WriteLine(sortedSet.Count);
+        Console.WriteLine(hashSet.Count);
     }
 }
