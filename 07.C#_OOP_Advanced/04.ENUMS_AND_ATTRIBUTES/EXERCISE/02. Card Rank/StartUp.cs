a433594f8909e7741d26516a815c8a781935fab6
Ilian Damianov <ilian.damianov@gmail.com>
2017-07-28 20:04:30 +0300

    Add blanks.

---
+++ 07.C#_OOP_Advanced/04.ENUMS_AND_ATTRIBUTES/EXERCISE/02. Card Rank/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CardRank
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ 07.C#_OOP_Advanced/04.ENUMS_AND_ATTRIBUTES/EXERCISE/09. Traffic Lights/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace TrafficLights
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
